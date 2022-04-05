fdf73802b0e129f2710e91325a318b9be82d646e
P1-ref <91056066+P1-ref@users.noreply.github.com>
2022-04-05 23:46:49 +0200

    Add files via upload

---
+++ Guess_the_number.py
@@ -0,0 +1,20 @@
+import random
+
+def main():
+    GUESSES = int(input("Hur många chanser vill du ha?\n"))
+    print(f"Du har {GUESSES} försök på dig.")
+    NUMBER = random.randint(1, 100)
+
+    for _ in range(GUESSES):
+        GUESS = int(input("Gissa numret mellan 1-100\n"))
+        if NUMBER == GUESS:
+            print("Du hade rätt!")
+        elif GUESS > NUMBER:
+            print("Numret är lägre!")
+        else:
+            print("Numret är större!")
+    print("Numret var", NUMBER)
+    CONTINUE = input("Vill du fortsätta?\n").lower()
+    main() if CONTINUE == "ja" else quit()
+
+main() if __name__ == "__main__" else print("Filen skall ej importeras, utan köras direkt.")
